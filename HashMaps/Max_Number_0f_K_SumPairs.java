df8471e2669a5b8778eb115a34b2d04892a15222
Ayush92003 <153195501+Ayush92003@users.noreply.github.com>
2024-07-12 10:20:54 +0530

    Create Max_Number_0f_K_SumPairs.java

---
+++ HashMaps/Max_Number_0f_K_SumPairs.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+public class Max_Number_of_K_Sum_Pairs {
+    public static void main(String[] args) {
+        int[]nums={1,2,3,4};
+        int  k = 5;
+        System.out.println(maxOperations(nums,k));
+    }
+    public static int maxOperations(int[] nums, int k) {
+        int cnt = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            int c = k - num;
+            if (map.containsKey(c)) {
+                cnt++;
+                if(map.get(c)==1){
+                    map.remove(c);
+                }
+                else{
+                    map.put(c, map.get(c) - 1);
+                }
+            } else {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+            }
+        }
+        return cnt;
+    }
+}
